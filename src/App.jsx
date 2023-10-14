e4f6622bbfabfe7c9040a12b9c5e3cb67126c4cb
Alaka A J <alaka03aj@gmail.com>
2023-10-14 18:15:14 +0530

    Added: Add Smoothie Form and DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from './pages/Home'
+import Create from './pages/Create'
 const App = () => {
   return (
     <div>
       <h1>Supa-Smoothies</h1>
       <Home />
+      <Create />
     </div>
   )
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import supabase from "../config/supabaseClient"
+import './Create.css'
 
 const Create = () => {
     const [name, setName] = useState('')
@@ -6,10 +8,42 @@ const Create = () => {
     const [rating, setRating] = useState('')
     const [formError, setFormError] = useState(null)
 
-    
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!name || !desc || !rating){
+        setFormError('Please fill all details before submitting!')
+        return
+    }
+
+    const {data, error } = await supabase.from('smoothies').insert([{name, desc, rating}])
+
+    if (error){
+        console.log(error)
+        setFormError(error)
+    }
+
+    if (data){
+        console.log(data)
+        setFormError(null)
+    }
+
+    console.log(name, desc, rating)
+}
+
   return (
-    <div>
-      
+    <div className="sform">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" value={name} onChange={(e) => {setName(e.target.value)}}/>
+        <label htmlFor="desc">Description: </label>
+        <input type="text" id="desc" value={desc} onChange={(e) => {setDesc(e.target.value)}}/>
+        <label htmlFor="desc">Rating: </label>
+        <input type="number" id="rating" value={rating} onChange={(e) => {setRating(e.target.value)}}/>
+
+        <button>Add Smoothie!</button>
+        {formError && <p>{formError}</p>}
+      </form>
     </div>
   )
 }
